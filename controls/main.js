276bfe0636d01288378437efa78b61f0333e2a62
Tythos <code@tythos.net>
2016-04-20 16:06:17 -0700

    Partial commit after editabletables controller added; still testing integration

--- controls/main.js
+++ controls/main.js
@@ -160,28 +160,47 @@ window.addEventListener("load", function() {
                 alert("Oh no! This shouldn't happen...");
         }
     };
+    var create = function(type) {
+        alert("Let's create a " + type + "!");
+    };
+    var del = function(type, name) {
+        alert("Let's delete " + type + " '" + name + "'");
+    };
     
     // Populate list of tables
     var dd = document.createElement('dd');
     dd.innerHTML = 'TABLES';
+    var st = document.createElement('span');
+    st.setAttribute('class', 'groupTitleAdd');
+    st.innerHTML = '[add]';
+    st.addEventListener('click', function(event) { create('table'); });
+    dd.appendChild(st);
     var dt = document.createElement('dt');
     var ul = document.createElement('ul');
     getTables().forEach(function(val,ndx) {
         var li = document.createElement('li');
-        var span = document.createElement('span');
-        span.innerHTML = '[edit]';
-        span.setAttribute('class', 'editButton');
+        var ed = document.createElement('span');
+        var dl = document.createElement('span');
+        ed.innerHTML = '[edit]';
+        ed.setAttribute('class', 'editButton');
+        dl.innerHTML = '[del]';
+        dl.setAttribute('class', 'delButton');
         li.innerHTML = val;
         li.addEventListener('click', function(event) {
             view('table', val);
             event.stopPropagation();
         });
-        span.addEventListener('click', function(event) {
+        ed.addEventListener('click', function(event) {
             //alert("Hey!");
             edit('table', val);
             event.stopPropagation();
         });
-        li.appendChild(span);
+        dl.addEventListener('click', function(event) {
+            del('table', val);
+            event.stopPropagation();
+        });
+        li.appendChild(ed);
+        li.appendChild(dl);
         ul.appendChild(li);
     });
     dt.appendChild(ul);
@@ -191,23 +210,36 @@ window.addEventListener("load", function() {
     // Populate list of queries
     var dd = document.createElement('dd');
     dd.innerHTML = 'QUERIES';
+    var st = document.createElement('span');
+    st.setAttribute('class', 'groupTitleAdd');
+    st.innerHTML = '[add]';
+    st.addEventListener('click', function(event) { create('query'); });
+    dd.appendChild(st);
     var dt = document.createElement('dt');
     var ul = document.createElement('ul');
     getQueries().forEach(function(val,ndx) {
         var li = document.createElement('li');
-        var span = document.createElement('span');
-        span.innerHTML = '[edit]';
-        span.setAttribute('class', 'editButton');
+        var ed = document.createElement('span');
+        var dl = document.createElement('span');
+        ed.innerHTML = '[edit]';
+        ed.setAttribute('class', 'editButton');
+        dl.innerHTML = '[del]';
+        dl.setAttribute('class', 'delButton');
         li.innerHTML = val;
         li.addEventListener('click', function(event) {
             view('query', val);
             event.stopPropagation();
         });
-        span.addEventListener('click', function(event) {
+        ed.addEventListener('click', function(event) {
             edit('query', val);
             event.stopPropagation();
         });
-        li.appendChild(span);
+        dl.addEventListener('click', function(event) {
+            del('query', val);
+            event.stopPropagation();
+        });
+        li.appendChild(ed);
+        li.appendChild(dl);
         ul.appendChild(li);
     });
     dt.appendChild(ul);
@@ -217,23 +249,36 @@ window.addEventListener("load", function() {
     // Populate list of figures
     var dd = document.createElement('dd');
     dd.innerHTML = 'FIGURES';
+    var st = document.createElement('span');
+    st.setAttribute('class', 'groupTitleAdd');
+    st.innerHTML = '[add]';
+    st.addEventListener('click', function(event) { create('figure'); });
+    dd.appendChild(st);
     var dt = document.createElement('dt');
     var ul = document.createElement('ul');
     getFigures().forEach(function(val,ndx) {
         var li = document.createElement('li');
-        var span = document.createElement('span');
-        span.innerHTML = '[edit]';
-        span.setAttribute('class', 'editButton');
+        var ed = document.createElement('span');
+        var dl = document.createElement('span');
+        ed.innerHTML = '[edit]';
+        ed.setAttribute('class', 'editButton');
+        dl.innerHTML = '[del]';
+        dl.setAttribute('class', 'delButton');
         li.innerHTML = val;
         li.addEventListener('click', function(event) {
             view('figure', val);
             event.stopPropagation();
         });
-        span.addEventListener('click', function(event) {
+        ed.addEventListener('click', function(event) {
             edit('figure', val);
             event.stopPropagation();
         });
-        li.appendChild(span);
+        dl.addEventListener('click', function(event) {
+            del('figure', val);
+            event.stopPropagation();
+        });
+        li.appendChild(ed);
+        li.appendChild(dl);
         ul.appendChild(li);
     });
     dt.appendChild(ul);
@@ -241,4 +286,25 @@ window.addEventListener("load", function() {
     e('#browserList').appendChild(dt);
     
     log('Finished loading');
+    
+    // TESTING EDITABLGRID
+    /*var eg = require('editablegrid');
+    editableGrid = new eg.EditableGrid("DemoGridAttach"); 
+    editableGrid.load({ metadata: [
+        { name: "name", datatype: "string", editable: true },
+        { name: "firstname", datatype: "string", editable: true },
+        { name: "age", datatype: "integer", editable: true },
+        { name: "height", datatype: "double(m,2)", editable: true },
+        { name: "country", datatype: "string", editable: true, values: 
+        { 'Europe': { "be" : "Belgium", "fr" : "France", "uk" : "Great-Britain", "nl": "Nederland"},
+        'America': { "br" : "Brazil", "ca": "Canada", "us" : "USA" },
+        'Africa': { "ng" : "Nigeria", "za": "South-Africa", "zw" : "Zimbabwe" }
+        }
+        },
+        { name: "email", datatype: "email", editable: true },
+        { name: "freelance", datatype: "boolean", editable: true },
+        { name: "lastvisit", datatype: "date", editable: true }
+    ]});
+    editableGrid.attachToHTMLTable('htmlgrid');
+    editableGrid.renderGrid();*/
 });
